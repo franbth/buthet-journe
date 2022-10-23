c6feed3fc8e0417b0b5b9b9cc2ec57fe5ebbd9f6
Francisco Buthet <franciscobuthet@gmail.com>
2022-10-23 20:50:29 -0300

    first commit

---
+++ js/productos.js
@@ -0,0 +1,58 @@
+const productos = [
+    {
+        id:1,
+        name:"Jordan 3 Retro Cardinal Red",
+        pago:"3 cuotas sin interes",
+        precio:149,
+        cantidadCompra:0,
+        img:"imagenes/Jordan-3-Retro-Cardinal-Red.jpg"
+    },
+    {
+        id:2,
+        name:"Jordan 4 Retro Canyon Purple",
+        pago:"3 cuotas sin interes",
+        precio:175,
+        cantidadCompra:0,
+        img:"imagenes/Jordan-4-Retro-Canyon-Purple-W.jpg"
+    },
+    {
+        id:3,
+        name:"Nike SB Dunk Low Club 58 Gulf",
+        pago:"3 cuotas sin interes",
+        precio:210,
+        cantidadCompra:0,
+        img:"imagenes/Nike-SB-Dunk-Low-Club-58-Gulf-1.jpg"
+    },
+    {
+        id:4,
+        name:"Adidas Yeezy Boost 350 V2 Bred",
+        pago:"3 cuotas sin interes",
+        precio:169,
+        cantidadCompra:0,
+        img:"imagenes/adidas-Yeezy-Boost-350-V2-Black-Red-2017-2020.jpg"
+    },
+    {
+        id:5,
+        name:"adidas Forum Low Bad Bunny Pink Easter Egg",
+        pago:"3 cuotas sin interes",
+        precio:249,
+        cantidadCompra:0,
+        img:"imagenes/adidas-Forum-Low-Bad-Bunny-Pink-Easter-Egg-.jpg"
+    },
+    {
+        id:6,
+        name:"adidas Yeezy Boost 700 Wave Runner Solid Grey",
+        pago:"3 cuotas sin interes",
+        precio:189,
+        cantidadCompra:0,
+        img:"imagenes/adidas-Yeezy-Boost-700-Wave-Runner-Solid-Grey.jpg"
+    },
+    {
+        id:7,
+        name:"Jordan 1 High Zoom Air CMFT Light Mauve",
+        pago:"3 cuotas sin interes",
+        precio:179,
+        cantidadCompra:0,
+        img:"imagenes/Jordan-1-High-Zoom-Air-CMFT-Light-Mauve-W.jpg"
+    },
+]
